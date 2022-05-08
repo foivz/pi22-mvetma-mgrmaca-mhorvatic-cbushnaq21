7ca94f9c01098e12a5cd2b3512154d8735cdc68d
valsim99 <mislav.vetma@gmail.com>
2022-05-08 19:05:32 +0200

    FrmHomePage created

--- Software/GymApp/GymForms/FrmHomePage.cs
+++ Software/GymApp/GymForms/FrmHomePage.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GymForms
 {
-    public partial class Form1 : Form
+    public partial class FrmHomePage : Form
     {
-        public Form1()
+        public FrmHomePage()
         {
             InitializeComponent();
         }
--- Software/GymApp/GymForms/Program.cs
+++ Software/GymApp/GymForms/Program.cs
@@ -16,7 +16,7 @@ namespace GymForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmHomePage());
         }
     }
 }
