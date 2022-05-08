87184cd79e5aed599fb5961b7da9214aa772126b
Marko128 <mgrmaca@foi.hr>
2022-05-08 21:43:02 +0200

    IUserController interface created.

---
+++ Software/GymApp/GymBussinessLogic/Interfaces/IUserController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymBussinessLogic.Interfaces
+{
+    public interface IUserController
+    {
+        bool AddNewUser(string name, string surname, string email, string password);
+        bool CheckUserCredentials(string email, string password);
+        bool UpdateUserName(string name, string surname, string email, string password);
+        bool DeleteuserName(string name, string surname, string email, string password);
+
+    }
+}
