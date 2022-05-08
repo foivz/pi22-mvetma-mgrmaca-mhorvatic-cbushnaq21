0cb1eb3654c22aa0acff7e23580af46077fb0cbe
Mhorvatic20 <Mhorvatic@foi.hr>
2022-05-08 13:04:06 -0700

    ICoachController interface created

---
+++ Software/GymApp/GymBussinessLogic/Interfaces/ICoachController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymBussinessLogic.Interfaces
+{
+    public interface ICoachController
+    {
+        bool AddNewCoach(string name, string surname, string education, string coachNumber, string coachEmail);
+        bool UpdateCoach(string name, string surname, string education, string coachNumber, string coachEmail);
+        bool DeleteCoach(string name, string surname, string education, string coachNumber, string coachEmail);
+
+    }
+}
