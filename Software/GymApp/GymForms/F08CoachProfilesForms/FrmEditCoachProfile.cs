26b6b2dd45c097cdea4841921b5fb51672fe8065
valsim99 <mvetma@foi.hr>
2022-06-20 17:23:36 +0200

    Edit profile for feature 8 (coach profiles) created

---
+++ Software/GymApp/GymForms/F08CoachProfilesForms/FrmEditCoachProfile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GymForms.F08CoachProfilesForms
+{
+    public partial class FrmEditCoachProfile : Form
+    {
+        public FrmEditCoachProfile()
+        {
+            InitializeComponent();
+        }
+    }
+}
