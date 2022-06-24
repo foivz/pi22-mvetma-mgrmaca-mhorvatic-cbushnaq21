fb814a9b07b5783861fee63b225e68c2a023d435
Mislav Vetma <mvetma@foi.hr>
2022-06-24 23:44:31 +0200

    Coach class created

---
+++ Software/GymApp/GymData/Coach.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymData
+{
+    public class Coach
+    {
+        public string CoachName { get; set; }
+        public string CoachSurname { get; set; }
+        public string CoachEducation { get; set; }
+        public string CoachNumber { get; set; }
+        public string CoachEmail { get; set; }
+
+        public Coach(string coachName, string coachSurname, string coachEducation, string coachNumber, string coachEmail)
+        {
+            CoachName = coachName;
+            CoachSurname = coachSurname;
+            CoachEducation = coachEducation;
+            CoachNumber = coachNumber;
+            CoachEmail = coachEmail;
+        }
+
+        public Coach()
+        {
+
+        }
+    }
+}
